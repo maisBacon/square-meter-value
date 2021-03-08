df00f42828588ef789c357e568c1fdf0bef72a95
renan <renan.eiras.melo@gmail.com>
2021-03-08 20:58:41 -0300

    first commit

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -2,7 +2,7 @@ require('../bootstrap');
 
 module.exports = {
   mongo: {
-    path: process.env.DB_PATH,
+    path: process.env.DB_PATH || 'mongodb://localhost:27017/square_meter_value',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
